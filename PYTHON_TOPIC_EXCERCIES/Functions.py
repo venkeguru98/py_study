fb80a6fc5e54d912cee9648b84e127eea4a4c7b9
venkat <priyavenke2@gmail.com>
2023-07-06 19:03:54 +0530

    function changes

--- PYTHON_TOPIC_EXCERCIES/Functions.py
+++ PYTHON_TOPIC_EXCERCIES/Functions.py
@@ -62,6 +62,100 @@ print("enter the number")
 
 #! Functions can be made in arguments 
 
+#Arguments are information passed in the functions 
+
+def afunction(name):
+    print("the name of the given " + " " ,name)
+
+afunction ("venke")
+afunction ("VENKAT")
+
+
+#! ir the number of args are undefined *args are used to access the function and when 
+# *args are used they act as tuple and accessed by the tuuple indexes 
+
+
+
+def argfunction (*names):
+    print("the Name to be displayed is ",names[0])
+    
+argfunction("venke","venkat","guru")
+
+
+#2 
+
+def alistfunction (*numbers):
+    for number in numbers:
+        print(number)
+    
+
+alist = [1,2,3,4,5]
+alistfunction(*alist)
+
+
+#KEYWORD ARGUMENT = ** double astreisk used in keyarguments 
+#synatax key = value 
+
+#1
+def keywordargument (value2,value1,value3):
+    print("The name of the student 2 is",value2)
+    
+    
+keywordargument(name="venke",name2="guru",name3 ="venkat")
+
+#2 - CALCULATE THE TOTAL COST OF ITEM with any discounr and taxes
+
+
+
+def calulate_the_total_cost (itemprice,qunatity,totalpercent=0,totaltax=0):
+    total_cost = itemprice * qunatity
+    total_cost -= total_cost * totalpercent/100
+    #total_cost = totalcost+totalpercent/100
+    total_cost += total_cost * totaltax /100
+    
+    return total_cost
+
+
+total = calulate_the_total_cost(100,5)
+print(total)
+
+#2 CALCULATE_AVERAGE
+
+def calculate_average (listvalues):
+    
+    sum =0
+    count =0 
+    for value in listvalues:
+        sum = sum + value 
+        count = count + 1
+        
+    average = sum /count 
+    return average    
+
+listvalues = [1,2,3,4,5]
+result = calculate_average(listvalues)
+print(result)
+
+
+#3 - arbitary keyword arguments 
+
+#if the number of keys words are unknown ** are used 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
