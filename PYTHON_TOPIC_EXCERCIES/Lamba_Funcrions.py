39476734e4807f1a302aa708d16326f07d0f0996
venkat <priyavenke2@gmail.com>
2023-07-13 23:08:38 +0530

    lambda functions

--- PYTHON_TOPIC_EXCERCIES/Lamba_Funcrions.py
+++ PYTHON_TOPIC_EXCERCIES/Lamba_Funcrions.py
@@ -3,4 +3,32 @@
 
 # SYNTAX : lamba arguments : expression 
 
+# 1. arguments are the input paramter for the function 
+# 2. Expression is the condition to be executed with the inputs 
+
+
+# Lambda can take any number of Arguments 
+
+x = lambda a : a + 10  
+print(x(5))
+
+x1 = lambda a,b,c : a+b+c
+print(x1(3,4,5))
+
+
+# Program to calculate Lambda using a Function and Lambda 
+# FUCNTION 
+
+def square_number(n):
+    
+    return n*n
+
+square_number(5)
+
+# using LAMBDA 
+
+square = lambda x : x **2 
+print(x(2))
+
+
 
