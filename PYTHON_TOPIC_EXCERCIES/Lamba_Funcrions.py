daf33c3b11a8bb77795b944b1808b1dd9da58adf
venkat <priyavenke2@gmail.com>
2023-07-16 22:44:51 +0530

    lambda

--- PYTHON_TOPIC_EXCERCIES/Lamba_Funcrions.py
+++ PYTHON_TOPIC_EXCERCIES/Lamba_Funcrions.py
@@ -1,4 +1,5 @@
 #LAmba Functions are small anomymns functions 
+# Lambda Functions which are used insde the another Functions 
 #! They are funtions that take any number of Arguments but one only expression 
 
 # SYNTAX : lamba arguments : expression 
@@ -27,8 +28,17 @@ square_number(5)
 
 # using LAMBDA 
 
-square = lambda x : x **2 
+square = lambda x : x ** 2  # a ** b ---> which means the a to the power of b 
 print(x(2))
 
 
+#sample program for using Lamba within a FUnction 
+
+def afunction (n):
+    return lambda a:a + n 
+
+avalue = afunction(7)
+print(avalue(7)) 
+
+#------------------------
 
